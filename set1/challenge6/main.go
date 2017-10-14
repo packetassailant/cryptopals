0122ceb0d2b21b633867f4e639ba659e91796f1a
chris patten <chris@stacktitan.com>
2017-10-14 17:04:13 -0400

    committing correct edit distance solution

--- set1/challenge6/main.go
+++ set1/challenge6/main.go
@@ -15,56 +15,76 @@ import (
 type cipherTexts struct {
 	ct1 string
 	ct2 string
+	ct3 string
+	ct4 string
+	ct5 string
 }
 
 var ctMap = map[int]*cipherTexts{}
 
 func generateKeys(s string) map[int]*cipherTexts {
-	// stats, err := f.Stat()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// var size = stats.Size()
 	b := []byte(s)
-	// bufReader := bytes.NewReader(f)
-	// _, err = bufReader.Read(b)
 	bReader := bytes.NewReader(b)
 
 	for i := 2; i <= 40; i++ {
 		keysize1 := make([]byte, i)
 		keysize2 := make([]byte, i)
+		keysize3 := make([]byte, i)
+		keysize4 := make([]byte, i)
+		keysize5 := make([]byte, i)
 		binary.Read(bReader, binary.BigEndian, keysize1)
 		binary.Read(bReader, binary.BigEndian, keysize2)
+		binary.Read(bReader, binary.BigEndian, keysize3)
+		binary.Read(bReader, binary.BigEndian, keysize4)
+		binary.Read(bReader, binary.BigEndian, keysize5)
 		ctMap[i] = &cipherTexts{
 			ct1: fmt.Sprintf("%s", keysize1),
 			ct2: fmt.Sprintf("%s", keysize2),
+			ct3: fmt.Sprintf("%s", keysize3),
+			ct4: fmt.Sprintf("%s", keysize4),
+			ct5: fmt.Sprintf("%s", keysize5),
 		}
-		// fmt.Printf("Key1: %s : %v\n", keysize1, len(keysize1))
-		// fmt.Printf("Key2: %s : %v\n", keysize2, len(keysize2))
 		bReader.Seek(0, 0)
 	}
 	return ctMap
 }
 
-func getHammingValue(bm map[int]string) int {
-	count := 0
-	bm1 := []rune(bm[0])
-	bm2 := []rune(bm[1])
+func getHammingValue(bm map[int]string, key int) float64 {
+	var (
+		c0  = 0.00
+		c1  = 0.00
+		c2  = 0.00
+		c3  = 0.00
+		avg = 0.00
+	)
 
-	for i := 0; i < len(bm1); i++ {
+	bm0 := []rune(bm[0])
+	bm1 := []rune(bm[1])
+	bm2 := []rune(bm[2])
+	bm3 := []rune(bm[3])
+	bm4 := []rune(bm[4])
+
+	for i := 0; i < len(bm0); i++ {
+		if bm0[i] != bm1[i] {
+			c0++
+		}
 		if bm1[i] != bm2[i] {
-			count++
+			c1++
+		}
+		if bm2[i] != bm3[i] {
+			c2++
+		}
+		if bm3[i] != bm4[i] {
+			c3++
 		}
 	}
-	return count
+	avg = (c0/float64(key) + c1/float64(key) + c2/float64(key) + c3/float64(key)) / 4
+	return avg
 }
 
 func keyToBin(s ...string) map[int]string {
 	binMap := make(map[int]string)
 
-	if len(s[0]) != len(s[1]) {
-		log.Fatal("Each string is not of even length")
-	}
 	for i := range s {
 		binStr := ""
 		for _, x := range s[i] {
@@ -88,7 +108,6 @@ func main() {
 	decFileStr := fmt.Sprintf("%s", decFileBytes)
 
 	cm := generateKeys(decFileStr)
-
 	var keys []int
 	for k := range cm {
 		keys = append(keys, k)
@@ -96,8 +115,14 @@ func main() {
 	sort.Ints(keys)
 	for _, k := range keys {
 		fmt.Printf("Processing key: %d\n", k)
-		binMap := keyToBin(cm[k].ct1, cm[k].ct2)
-		hamCount := getHammingValue(binMap)
-		fmt.Printf("Hamming Distance: %v\n", float64(hamCount)/float64(k))
+		binMap := keyToBin(
+			cm[k].ct1,
+			cm[k].ct2,
+			cm[k].ct3,
+			cm[k].ct4,
+			cm[k].ct5,
+		)
+		hamCount := getHammingValue(binMap, k)
+		fmt.Printf("Hamming Distance: %.4f\n", hamCount)
 	}
 }
