a593c8ddbfe021346ab468dcee7bd70f75687ca4
packetassailant <cpatten@packetresearch.com>
2017-10-23 19:02:53 -0400

    Committing solution for set 1 challenge 8

--- set1/challenge8/main.go
+++ set1/challenge8/main.go
@@ -1,10 +1,17 @@
+// Code to detect AES in ECB mode
+// 1. STDIN file of hex encoded ciphertexts
+// 2. Hex decode to byte slice
+// 3. Read each ciphertext into a slice of keyblock length slices
+// 4. Iterate and compare each ciphertexts keyblock slices to test for equality
+
 package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
-	"io"
 	"os"
+	"reflect"
 )
 
 func readFile(fn string) (err error) {
@@ -14,24 +21,25 @@ func readFile(fn string) (err error) {
 	if err != nil {
 		return err
 	}
+	scanner := bufio.NewScanner(file)
 
-	reader := bufio.NewReader(file)
-
-	var line string
-	for {
-		line, err = reader.ReadString('\n')
-
-		fmt.Printf(" > Read %d characters\n", len(line))
-
-		if err != nil {
-			break
+	blocksize := 16
+	for scanner.Scan() {
+		line := scanner.Text()
+		ciphertext, _ := hex.DecodeString(line)
+		fmt.Println("Processing Line: ", line)
+		var cipherSlice [][]byte
+		for bs, be := 0, blocksize; bs < len(ciphertext); bs, be = bs+blocksize, be+blocksize {
+			cipherSlice = append(cipherSlice, ciphertext[bs:be])
+		}
+		for a := 0; a < len(cipherSlice)-1; a++ {
+			for b := a + 1; b < len(cipherSlice); b++ {
+				if reflect.DeepEqual(cipherSlice[a], cipherSlice[b]) {
+					fmt.Printf("Found a block match of %v at postion %d\n", cipherSlice[a], b)
+				}
+			}
 		}
 	}
-
-	if err != io.EOF {
-		fmt.Printf(" > Failed!: %v\n", err)
-	}
-
 	return
 }
 
