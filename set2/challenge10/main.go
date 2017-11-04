95f4e92fa8067bb63b7402a6bb31045d2f602e34
packetassailant <cpatten@packetresearch.com>
2017-11-04 18:38:18 -0400

    Committing solution for set2 challenge10

--- set2/challenge10/main.go
+++ set2/challenge10/main.go
@@ -1,4 +1,4 @@
-// Code to implement AES ECB
+// Code to implement AES CBC (static IV of []byte* 16)
 // 1. Base64 decode to get ciphertext
 // 2. Decrypt ciphertext (AES 128) with 16 byte key
 
@@ -6,56 +6,96 @@ package main
 
 import (
 	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 )
 
-func encryptAes128(plaintext, key, ciphertext []byte) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
+func encryptAes(plaintext, key []byte) (ciphertext []byte, ciphertextB64 string, err error) {
+	if len(plaintext)%aes.BlockSize != 0 {
+		panic("plaintext is not a multiple of block size")
 	}
-	fmt.Printf("%d bytes NewCipher key with block size of %d bytes\n", len(key), block.BlockSize())
-	iv := ciphertext[:aes.BlockSize]
-	fmt.Println(iv)
-}
-
-func decryptAes128(ciphertext, key []byte) []byte {
-	cipher, err := aes.NewCipher(key)
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		log.Fatal(err)
 	}
-	plaintext := make([]byte, len(ciphertext))
-	blocksize := 16
-
-	for bs, be := 0, blocksize; bs < len(ciphertext); bs, be = bs+blocksize, be+blocksize {
-		cipher.Decrypt(plaintext[bs:be], ciphertext[bs:be])
+	ciphertext = make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
 	}
-	return plaintext
+	fmt.Printf("CBC Key: %s\n", hex.EncodeToString(key))
+	fmt.Printf("CBC IV: %s\n", hex.EncodeToString(iv))
+
+	cbc := cipher.NewCBCEncrypter(block, iv)
+	cbc.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	ciphertextB64 = base64.URLEncoding.EncodeToString(ciphertext)
+
+	return
+}
+
+func decryptAes(ciphertext, key []byte) (plaintext []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+	if len(ciphertext) < aes.BlockSize {
+		fmt.Printf("ciphertext is too short")
+		return
+	}
+	// Typical implementation would
+	// * Have an initial 0'th rand IV
+	// * Slice the IV from [:ciphertext]
+	// * Slice the ciphertext [ciphertext:]
+
+	iv := make([]byte, 16)
+
+	cbc := cipher.NewCBCDecrypter(block, iv)
+	cbc.CryptBlocks(ciphertext, ciphertext)
+	plaintext = ciphertext
+
+	return
 }
 
 func main() {
 	plaintext := flag.String("plaintext", "", "the plaintext to encrypt")
-	ciphertext := flag.String("ciphertext", "", "the ciphertext to decrypt")
+	ciphertext := flag.String("ciphertext", "", "the base64 encoded ciphertext to decrypt")
+	file := flag.String("file", "", "the file to decrypt")
 	key := flag.String("key", "", "the encryption/decryption key")
-	// iv := flag.String("iv", "", "The initialization vector for the block chain")
-	// encrypt := flag.Bool("encrypt", false, "encrypt")
-	// decrypt := flag.Bool("decrypt", false, "decrypt")
 	flag.Parse()
 
-	encryptAes128([]byte(*plaintext), []byte(*key), []byte(*ciphertext))
+	// go run main.go -plaintext='HHHHHello World!' -key="opensesame123456"
+	if *plaintext != "" && *key != "" {
+		result, resultB64, _ := encryptAes([]byte(*plaintext), []byte(*key))
+		fmt.Printf("B 64: %v\n", resultB64)
+		fmt.Printf("Bytes: %v\n", result)
+	}
 
-	// file := flag.String("file", "", "the file to decrypt")
-	// key := flag.String("key", "", "The decryption key")
-	// flag.Parse()
+	// go run main.go -ciphertext='jCIQ0BlqXYh9DIhcgfgyA5fHVd7H2o7Q87G37hXjYc4=' -key="opensesame123456"
+	if *ciphertext != "" && *key != "" {
+		sDec, _ := base64.StdEncoding.DecodeString(*ciphertext)
+		result, _ := decryptAes(sDec, []byte(*key))
+		fmt.Printf("Bytes: %v\n", result)
+		fmt.Printf("String: %s\n", result)
+	}
 
-	// fileBytes, err := ioutil.ReadFile(*file)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// k := []byte(*key)
+	// go run main.go -file=raw.txt -key="YELLOW SUBMARINE"
+	if *file != "" && *key != "" {
+		fileBytes, err := ioutil.ReadFile(*file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		k := []byte(*key)
 
-	// result := decryptAes128(fileBytes, k)
-	// fmt.Printf("%s", result)
+		result, _ := decryptAes(fileBytes, k)
+		fmt.Println(string(result))
+	}
+
 }
