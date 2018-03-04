7b5c6cf6a57fa1544152d743c9522ea35b234178
packetassailant <cpatten@packetresearch.com>
2018-03-04 18:11:40 -0500

    Reworked the solution to actually be correct this time

--- set2/challenge12/main.go
+++ set2/challenge12/main.go
@@ -1,4 +1,4 @@
-// byte at a time ECB decrypt
+// byte at a time ECB decrypt (easier)
 // 1. Detect block size by prepadding the ciphertext until the length increases (count new bytes up until increase)
 // 2. Detect unknown string size by prepadding the ciphertext until the length increases (count increase and round up for blocksize padding)
 // 3. Detect ECB by feeding in at least 32 bytes of deterministic padding ("YELLOW SUBMARINEYELLOW SUBMARINE")
@@ -22,11 +22,16 @@ import (
 	"github.com/packetassailant/cryptopals/rand"
 )
 
-var key = rand.String(16)
+var (
+	key         = rand.String(16)
+	unknownText = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" +
+		"aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq" +
+		"dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+)
 
-func encryptionOracle(plaintext, base64text string) (ciphertext []byte, err error) {
-	ptBytes := []byte(plaintext)
-	plainBytes, err := base64.StdEncoding.DecodeString(base64text)
+func encryptionOracle(controlChars string) (ciphertext []byte, err error) {
+	ptBytes := []byte(controlChars)
+	plainBytes, err := base64.StdEncoding.DecodeString(unknownText)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,56 +39,55 @@ func encryptionOracle(plaintext, base64text string) (ciphertext []byte, err erro
 	return crypt.EncryptAesECB(ptBytes, []byte(key))
 }
 
-func detectBlockSize(initSize int, b string) int {
+func detectBlockSize() int {
+	ciphertextOrig, _ := encryptionOracle("")
 	count := 1
 	//Start pre-padding the ciphertext
 	for {
 		a := strings.Repeat("A", count)
-		ciphertext, err := encryptionOracle(a, b)
+		ciphertextNew, err := encryptionOracle(a)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if len(ciphertext) > initSize {
-			return len(ciphertext) - initSize
+		if len(ciphertextNew) > len(ciphertextOrig) {
+			return len(ciphertextNew) - len(ciphertextOrig)
 		}
 		count++
 	}
 }
 
-func detectOracleSize(initSize int, b string) int {
+func detectOracleSize() int {
+	ciphertextOrig, _ := encryptionOracle("")
 	count := 1
 	//Start pre-padding the ciphertext
 	for {
 		a := strings.Repeat("A", count)
-		ciphertext, err := encryptionOracle(a, b)
+		ciphertextNew, err := encryptionOracle(a)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if len(ciphertext) > initSize {
-			return len(ciphertext) - count
+		if len(ciphertextNew) > len(ciphertextOrig) {
+			return len(ciphertextOrig) - count
 		}
 		count++
 	}
 }
 
-func bruteOracle(plaintext string, oraclesize, blocksize int) []byte {
-	oracleSizeRnd := (oraclesize/blocksize + 1) * blocksize
-	var unknownResult = []byte{}
-	fmt.Println("-------Bruting Oracle-------")
-	for x := oracleSizeRnd - 1; x > 0; x-- {
+func bruteOracle(blocksize, oraclesize int) []byte {
+	var unknownResult []byte
+	unknownStringSizeRnd := ((oraclesize / blocksize) + 1) * blocksize
+	for x := unknownStringSizeRnd - 1; x > 0; x-- {
 		bruteChars := strings.Repeat("A", x)
-		bruteBlockOne, _ := encryptionOracle(bruteChars, plaintext)
-		for v := 0; v <= 256; v++ {
+		ciphertextOne, _ := encryptionOracle(bruteChars)
+		ciphertextOneA := ciphertextOne[:unknownStringSizeRnd]
+		for char := 0; char < 256; char++ {
 			seedChars := []byte(bruteChars)
 			seedChars = append(seedChars, unknownResult...)
-			seedChars = append(seedChars, []byte(string(v))...)
-
-			bruteBlockTwo, _ := encryptionOracle(string(seedChars), plaintext)
-			if len(bruteBlockOne) < oracleSizeRnd {
-				return unknownResult
-			}
-			if bytes.Compare(bruteBlockOne[:oracleSizeRnd], bruteBlockTwo[:oracleSizeRnd]) == 0 {
-				unknownResult = append(unknownResult, []byte(string(v))...)
+			seedChars = append(seedChars, []byte(string(char))...)
+			ciphertextTwo, _ := encryptionOracle(string(seedChars))
+			ciphertextTwoA := ciphertextTwo[:unknownStringSizeRnd]
+			if bytes.Equal(ciphertextOneA, ciphertextTwoA) {
+				unknownResult = append(unknownResult, []byte(string(char))...)
 				break
 			}
 		}
@@ -92,24 +96,22 @@ func bruteOracle(plaintext string, oraclesize, blocksize int) []byte {
 }
 
 func main() {
-	a := "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-	b := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" +
-		"aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq" +
-		"dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
-	initCiphertext, err := encryptionOracle("", b)
-	ciphertext, err := encryptionOracle(a, b)
-	if err != nil {
-		log.Fatal(err)
-	}
+	controlChars := strings.Repeat("A", 64)
+	ciphertext, err := encryptionOracle(controlChars)
+
 	blockmode := crypt.DetectOracle(ciphertext, len(key))
 	fmt.Println(blockmode)
 
-	blocksize := detectBlockSize(len(initCiphertext), b)
+	blocksize := detectBlockSize()
 	fmt.Println("Detected blocksize of:", blocksize)
 
-	oraclesize := detectOracleSize(len(initCiphertext), b)
+	oraclesize := detectOracleSize()
 	fmt.Println("Detected string size of:", oraclesize)
 
-	ukns := bruteOracle(b, oraclesize, blocksize)
-	fmt.Println("The unknown string is: ", string(ukns))
+	resultString := bruteOracle(blocksize, oraclesize)
+	fmt.Println(string(resultString))
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
